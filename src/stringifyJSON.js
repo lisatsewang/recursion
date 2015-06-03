ab504a4f417035bafb4c93fa45c332aa311092f7
lisatsewang <lisatsewang@gmail.com>
2015-06-03 00:31:27 -0700

    stringifyJSON pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+  // JSON string to be returned
+  var resultArray = [];
+
+  // check for number, result: "5"
+  if (typeof obj === "number") { return obj.toString(); }
+
+  // check for string, result: ""string""
+  if (typeof obj === "string") { return ('"' + obj + '"'); }
+
+  // check for boolean, result: "true"
+  if (typeof obj === "boolean") { return obj.toString();; }
+
+  // check for null, result: "null", "{"null":null}"
+  if (obj === null) { return "null"; }
+
+  // if value === undefined or function, do nothing
+
+  // check for array, result: "[1, "s"]"
+  if (Array.isArray(obj)) {
+
+    for (var i = 0, arrayLength = obj.length; i < arrayLength; i++) {
+
+      resultArray.push(stringifyJSON(obj[i])); 
+    }
+
+    return "[" + resultArray + "]"; 
+  }
+
+  // check for object
+  if (typeof obj === "object") {
+
+    for (var key in obj) {
+      
+      // only push if object key has a defined value
+      if ((obj[key] !== undefined) && (key !== "functions")) {
+
+        var keyString = '"' + key + '"';
+        resultArray.push(keyString + ":" + stringifyJSON(obj[key]));
+      }
+    }
+
+    return "{" + resultArray + "}";
+  }
+}; 
